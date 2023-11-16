83240ab27586300d50d4828c7329c60550e8b536
Bae <bjy556@gmail.com>
2023-11-16 10:35:37 +0900

    db url add, workspace delete

--- workspace/routes/prompts.js
+++
@@ -1,9 +0,0 @@
-const PromController = require("../controllers/prompts");
-const express = require("express");
-const router = express.Router();
-
-
-//값 받는 router
-router.post('/getPrompt',PromController.prompt);
-
-module.exports = router;
--- workspace/routes/storys.js
+++
@@ -1,9 +0,0 @@
-const StoryController = require("../controllers/storys");
-const express = require("express");
-const router = express.Router();
-
-
-//값 받는 router
-router.post('/story',StoryController.story);
-
-module.exports = router;
